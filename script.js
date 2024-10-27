7d008d52075a12e109723acb18b24973f1f1b9f3
Kienosis <steventcull@gmail.com>
2024-10-27 17:50:06 -0400

    photos

--- script.js
+++ script.js
@@ -87,6 +87,28 @@ document.addEventListener('mousemove', (e) => {
     parallax.style.backgroundPosition = `${x}% ${y}%`;
 });
 
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true
+  },
+  spaceBetween: 60,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+
+
+
 
 
 
